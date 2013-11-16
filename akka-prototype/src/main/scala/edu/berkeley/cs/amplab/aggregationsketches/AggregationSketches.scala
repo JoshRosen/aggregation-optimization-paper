1e1ed8f197c45ea2a208632f786910763c601c0b
Josh Rosen <rosenville@gmail.com>
2013-11-15 16:05:58 -0800

    More data generators.

--- akka-prototype/src/main/scala/edu/berkeley/cs/amplab/aggregationsketches/AggregationSketches.scala
+++ akka-prototype/src/main/scala/edu/berkeley/cs/amplab/aggregationsketches/AggregationSketches.scala
@@ -7,28 +7,35 @@ import edu.berkeley.cs.amplab.aggregationsketches.aggregators._
 object AggregationSketches {
 
   def main(args: Array[String]) {
-    val numItems = 20000
-    val maxKey = 40000
-    val dataGenerators = Seq(1.3).map{ alpha =>
+    val numItems = 1000000
+    val maxKey   = 500000
+    val dataGenerators = (Seq(1.1, 1.3, 1.5, 1.7).map{ alpha =>
       "Zipf (alpha=%s)".format(alpha) -> DataGenerators.zipf(alpha, maxKey = maxKey)
-    }.toMap
+    } ++ Seq(
+      "Uniform" -> DataGenerators.uniform(maxKey),
+      "80% Heavy Hitter" -> DataGenerators.heavyHitter(0.8f),
+      "50% Heavy Hitter" -> DataGenerators.heavyHitter(0.5f),
+      "20% Heavy Hitter" -> DataGenerators.heavyHitter(0.2f)
+    )).toMap
     // "All Unique" -> DataGenerators.countFrom(1)
     // TODO: log the number of unique keys and some other properties of the dataset
 
     val output = CSVWriter(System.out)
     // Outputs results as TSV, which can be pasted into Excel for analysis:
-    val columnNames = Seq("Data Set", "Num Items", "Output Size", "Time (ms)",
+    val columnNames = Seq("Data Set", "Num Items", "Num Unique Keys", "Num Singletons", "Output Size", "Output Percentage", "Time (ms)",
       "RamUsageEstimator (Bytes)", "Back-of-envelope memory usage (Bytes)", "Aggregation Plan")
     output.write(columnNames)
 
     for ((generatorName, generator) <- dataGenerators) yield {
+      println("\n\nGenerating for generator " + generatorName)
       val items = generator.take(numItems)
       val numUniqueKeys = items.toSet.size
+      val numSingletonKeys = items.groupBy(identity).mapValues(_.size).count(_._2 == 1)
       implicit def aggregationFunction(a: Int, b: Int): Int = a + b
       val aggregationPlans: Seq[AggregationPlan[Int, Int]] = Seq(
         {
           val sink = new CountAggregator
-          val root = new RandomBufferingPreAggregator[Int, Int](numItems, sink)
+          val root = new RandomBufferingPreAggregator[Int, Int](numUniqueKeys, sink)
           new AggregationPlan("Complete pre-aggregation", root, sink)
         },
         {
@@ -37,24 +44,42 @@ object AggregationSketches {
         },
         {
           val sink = new CountAggregator
-          val root = new LRUBufferingPreAggregator[Int, Int](math.round(0.01f * numUniqueKeys), sink)
-          new AggregationPlan("1% LRU Buffer", root, sink)
+          val root = new LRUBufferingPreAggregator[Int, Int](math.round(0.20f * numUniqueKeys), sink)
+          new AggregationPlan("20% LRU Buffer", root, sink)
         },
         {
           val sink = new CountAggregator
-          val lru = new LRUBufferingPreAggregator[Int, Int](math.round(0.01f * numUniqueKeys), sink)
+          val root = new LRUBufferingPreAggregator[Int, Int](math.round(0.40f * numUniqueKeys), sink)
+          new AggregationPlan("40% LRU Buffer", root, sink)
+        },
+        {
+          val sink = new CountAggregator
+          val lru = new LRUBufferingPreAggregator[Int, Int](math.round(0.20f * numUniqueKeys), sink)
           val root = new BloomFilterRouter[Int, Int](1000, 0.01f, 0.5f)(lru, sink)
-          new AggregationPlan("1000-key Bloom Filter + 1% LRU Buffer", root, sink)
+          new AggregationPlan("1000-key Bloom Filter + 20% LRU Buffer", root, sink)
         },
         {
           val sink = new CountAggregator
-          val root = new CountMinSketchBufferingPreAggregator[Int, Int](0.01, 1E-3)(math.round(0.01f * numUniqueKeys), sink)
-          new AggregationPlan("1% CMS Buffer", root, sink)
+          val lru = new LRUBufferingPreAggregator[Int, Int](math.round(0.20f * numUniqueKeys), sink)
+          val root = new BloomFilterRouter[Int, Int](10000, 0.01f, 0.5f)(lru, sink)
+          new AggregationPlan("10000-key Bloom Filter + 20% LRU Buffer", root, sink)
         },
         {
           val sink = new CountAggregator
-          val root = new FIFOBufferingPreAggregator[Int, Int](math.round(0.01f * numUniqueKeys), sink)
-          new AggregationPlan("1% FIFO Buffer", root, sink)
+          val lru = new LRUBufferingPreAggregator[Int, Int](math.round(0.20f * numUniqueKeys), sink)
+          val root = new BloomFilterRouter[Int, Int](numUniqueKeys, 0.01f, 0.5f)(lru, sink)
+          new AggregationPlan("#uniqueKeys Bloom Filter + 20% LRU Buffer", root, sink)
+        },
+        {
+          val sink = new CountAggregator
+          val lru = new LRUBufferingPreAggregator[Int, Int](math.round(0.10f * numUniqueKeys), sink)
+          val root = new BloomFilterRouter[Int, Int](numUniqueKeys, 0.01f, 0.5f)(lru, sink)
+          new AggregationPlan("#uniqueKeys Bloom Filter + 10% LRU Buffer", root, sink)
+        },
+        {
+          val sink = new CountAggregator
+          val root = new FIFOBufferingPreAggregator[Int, Int](math.round(0.20f * numUniqueKeys), sink)
+          new AggregationPlan("20% FIFO Buffer", root, sink)
         }
       )
 
@@ -74,7 +99,7 @@ object AggregationSketches {
       }
 
       for ((plan, numOutputTuples, time, policySize) <- stats) {
-        output.write(Seq(generatorName, numItems, numOutputTuples, time, policySize, plan.memoryUsageInBytes, plan.name).map(_.toString))
+        output.write(Seq(generatorName, numItems, numUniqueKeys, numSingletonKeys, numOutputTuples, numOutputTuples * 1.0f / numItems, time, policySize, plan.memoryUsageInBytes, plan.name).map(_.toString))
         output.flush()
       }
     }
--- akka-prototype/src/main/scala/edu/berkeley/cs/amplab/aggregationsketches/DataGenerators.scala
+++ akka-prototype/src/main/scala/edu/berkeley/cs/amplab/aggregationsketches/DataGenerators.scala
@@ -1,10 +1,35 @@
 package edu.berkeley.cs.amplab.aggregationsketches
 
 import org.apache.hadoop.io.file.tfile.RandomDistribution
+import scala.util.Random
 
 object DataGenerators {
   def zipf(alpha: Double, minKey: Int = 1, maxKey: Int = 100, seed: Int = 42): Stream[Int] = {
     val z = new RandomDistribution.Zipf(new java.util.Random(seed), minKey, maxKey, alpha)
     Stream.continually(z.nextInt)
   }
+
+  def uniform(maxKey: Int, seed: Int = 42): Stream[Int] = {
+    val r = new Random(seed)
+    Stream.continually(r.nextInt(maxKey))
+  }
+
+  /**
+   * Distribution where one key is responsible for most of the records and other keys appear only once.
+   */
+  def heavyHitter(probabilityOfHeavyHitterKey: Float, seed: Int = 42): Stream[Int] = {
+    val r = new Random(seed)
+    val heavyHitterKey: Int = 1
+    var nextUniqueKey: Int = 1
+    Stream.continually {
+      if (r.nextFloat() < probabilityOfHeavyHitterKey) {
+        heavyHitterKey
+      } else {
+        nextUniqueKey += 1
+        nextUniqueKey
+      }
+    }
+  }
+
+
 }
